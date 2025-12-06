39d8352db05cf3afd315993b6653cc7855e985d3
adamadiga90 <atheadigaman83@gmail.com>
2025-12-06 12:44:35 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="bg-blue-400 text-amber-600  text-2xl p-10">hello</div>
+      <div className="bg-blue-500 text-amber-600 font-bold  text-2xl p-10">
+        hello
+      </div>
+    </>
+  );
+};
+
+export default page;
